9d5a34f508cf189e267cfb2c8fed06c641f8c338
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-11-06 18:28:08 +0700

    done

---
+++ src/main/java/cg/wbd/grandemonstration/SpringbootCustomerManagementWebappApplication.java
@@ -0,0 +1,13 @@
+package cg.wbd.grandemonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomerManagementWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCustomerManagementWebappApplication.class, args);
+    }
+
+}
